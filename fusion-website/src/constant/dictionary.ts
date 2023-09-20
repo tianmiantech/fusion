452ca01386256ab14c98490a300903da18529f3b
flos <flos.guo@welab-inc.com>
2023-09-20 11:18:30 +0800

    feat: task

---
+++ fusion-website/src/constant/dictionary.ts
@@ -0,0 +1,11 @@
+// 样本类型
+export const dataResourceTypeMap = new Map([
+  ['DataSet', '数据集'],
+  ['BloomFilter', '布隆过滤器'],
+]);
+
+// 样本选择方式
+export const dataSetAddMethodMap = new Map([
+  ['file', '选择文件'],
+  ['sql', '数据库'],
+]);
