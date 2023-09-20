9139b0cb1f1f5651e062610c435958f4d64a2856
terryChu <184195136@qq.com>
2023-09-20 11:19:18 +0800

    首页,修改样式

--- fusion-website/src/pages/home/index.tsx
+++ fusion-website/src/pages/home/index.tsx
@@ -4,7 +4,7 @@ const Index = ()=>{
 
     return <div className={styles.container}>
         <h3>欢迎使用数据融合工具</h3>
-        <Button type="primary">发起任务</Button>
+        <Button className={styles.btnStyle} size='large' type="primary">发起任务</Button>
     </div>
 }
 export default Index
