431ef7983c5cfeeb3e85532c14bdc4aab79fb450
terry.zhu <terry.zhu@welab-inc.com>
2024-01-09 15:14:14 +0800

    修改api接口

--- fusion-website/src/utils/request.ts
+++ fusion-website/src/utils/request.ts
@@ -33,7 +33,7 @@ export const extractFirstPathAfterOrigin = (url: string)=>{
 // 全局变量
 export function getRequestBaseURL(){
   //return "http://172.31.21.36:9090/fusion"
-  return "https://xbd-dev.tianmiantech.com/fusion-01"
+  //return "https://xbd-dev.tianmiantech.com/fusion-01"
   if(window._wefeApi){
       /** 提供给客户快速修改请求地址，一般通过修改html head */
       return window._wefeApi;
